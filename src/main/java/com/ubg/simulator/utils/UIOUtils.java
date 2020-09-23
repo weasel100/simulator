274b0c7a4eb7cb043db208b924ca3cd45a8502f5
BRUCE.H <1qazxsw2@>
2020-09-23 18:34:51 +0800

    修正一版可以在idea执行

--- src/main/java/com/ubg/simulator/utils/UIOUtils.java
+++ src/main/java/com/ubg/simulator/utils/UIOUtils.java
@@ -1,10 +1,15 @@
 package com.ubg.simulator.utils;
-import java.io.InputStream;
+
+import java.io.*;
+
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.springframework.util.ResourceUtils;
+
 /**
  * @author bruce
- * */
+ */
 @Slf4j
 public class UIOUtils {
     /**
@@ -20,11 +25,21 @@ public class UIOUtils {
     public static String readFile(String classpathFile, String encoding) {
         String result = null;
         try {
-            log.info(classpathFile);
-            ClassLoader classLoader = UIOUtils.class.getClassLoader();
-            result = IOUtils.toString(classLoader.getResourceAsStream("response/" + classpathFile), encoding);
+            String path = ResourceUtils.getFile("response/" + classpathFile).getAbsoluteFile().getPath();
+            log.info("优先读JAR包同级目录下：" + path);
+            result = FileUtils.readFileToString(new File(path), encoding);
         } catch (Exception e) {
             log.error(e.getMessage());
+        }
+        //如果jar包外没有文件
+        if (result == null) {
+            try {
+                log.info("优先读JAR包内资源文件：" + classpathFile);
+                ClassLoader classLoader = UIOUtils.class.getClassLoader();
+                result = IOUtils.toString(classLoader.getResourceAsStream("response/" + classpathFile), encoding);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
         }
         return result;
     }
